9d29066ab83636f6fe65ed01017ca877e037a2a8
Petr E. Antonov <antpeter@gmail.com>
2019-05-16 12:04:35 +0300

    Marking forms. User.

--- vshaurme/forms/user.py
+++ vshaurme/forms/user.py
@@ -3,19 +3,20 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, HiddenField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Optional, Regexp
+from flask_babel import lazy_gettext as _l
 
 from vshaurme.models import User
 
 
 class EditProfileForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 30)])
-    username = StringField('Username', validators=[DataRequired(), Length(1, 20),
+    name = StringField(_l('Name'), validators=[DataRequired(), Length(1, 30)])
+    username = StringField(_l('Username'), validators=[DataRequired(), Length(1, 20),
                                                    Regexp('^[a-zA-Z0-9]*$',
                                                           message='The username should contain only a-z, A-Z and 0-9.')])
-    website = StringField('Website', validators=[Optional(), Length(0, 255)])
-    location = StringField('City', validators=[Optional(), Length(0, 50)])
-    bio = TextAreaField('Bio', validators=[Optional(), Length(0, 120)])
-    submit = SubmitField()
+    website = StringField(_l('Website'), validators=[Optional(), Length(0, 255)])
+    location = StringField(_l('City'), validators=[Optional(), Length(0, 50)])
+    bio = TextAreaField(_l('Bio'), validators=[Optional(), Length(0, 120)])
+    submit = SubmitField(_l('Submit'))
 
     def validate_username(self, field):
         if field.data != current_user.username and User.query.filter_by(username=field.data).first():
@@ -23,11 +24,11 @@ class EditProfileForm(FlaskForm):
 
 
 class UploadAvatarForm(FlaskForm):
-    image = FileField('Upload', validators=[
+    image = FileField(_l('Upload'), validators=[
         FileRequired(),
-        FileAllowed(['jpg', 'png'], 'The file format should be .jpg or .png.')
+        FileAllowed(['jpg', 'png'], _l('The file format should be .jpg or .png.'))
     ])
-    submit = SubmitField()
+    submit = SubmitField(_l('Submit'))
 
 
 class CropAvatarForm(FlaskForm):
@@ -35,42 +36,42 @@ class CropAvatarForm(FlaskForm):
     y = HiddenField()
     w = HiddenField()
     h = HiddenField()
-    submit = SubmitField('Crop and Update')
+    submit = SubmitField(_l('Crop and Update'))
 
 
 class ChangeEmailForm(FlaskForm):
-    email = StringField('New Email', validators=[DataRequired(), Length(1, 254), Email()])
-    submit = SubmitField()
+    email = StringField(_l('New Email'), validators=[DataRequired(), Length(1, 254), Email()])
+    submit = SubmitField(_l('Submit'))
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data.lower()).first():
-            raise ValidationError('The email is already in use.')
+            raise ValidationError(_l('The email is already in use.'))
 
 
 class ChangePasswordForm(FlaskForm):
-    old_password = PasswordField('Old Password', validators=[DataRequired()])
-    password = PasswordField('New Password', validators=[
+    old_password = PasswordField(_l('Old Password'), validators=[DataRequired()])
+    password = PasswordField(_l('New Password'), validators=[
         DataRequired(), Length(8, 128), EqualTo('password2')])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
-    submit = SubmitField()
+    password2 = PasswordField(_l('Confirm Password'), validators=[DataRequired()])
+    submit = SubmitField(_l('Submit'))
 
 
 class NotificationSettingForm(FlaskForm):
-    receive_comment_notification = BooleanField('New comment')
-    receive_follow_notification = BooleanField('New follower')
-    receive_collect_notification = BooleanField('New collector')
-    submit = SubmitField()
+    receive_comment_notification = BooleanField(_l('New comment'))
+    receive_follow_notification = BooleanField(_l('New follower'))
+    receive_collect_notification = BooleanField(_l('New collector'))
+    submit = SubmitField(_l('Submit'))
 
 
 class PrivacySettingForm(FlaskForm):
-    public_collections = BooleanField('Public my collection')
-    submit = SubmitField()
+    public_collections = BooleanField(_l('Public my collection'))
+    submit = SubmitField(_l('Submit'))
 
 
 class DeleteAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 20)])
-    submit = SubmitField()
+    username = StringField(_l('Username'), validators=[DataRequired(), Length(1, 20)])
+    submit = SubmitField(_l('Submit'))
 
     def validate_username(self, field):
         if field.data != current_user.username:
-            raise ValidationError('Wrong username.')
+            raise ValidationError(_l('Wrong username.'))
