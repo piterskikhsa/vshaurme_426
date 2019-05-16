0ca4f0bb67c3841ba8943090e61212247921f3a2
Petr E. Antonov <antpeter@gmail.com>
2019-05-16 12:00:09 +0300

    Marking forms. Main.

--- vshaurme/forms/main.py
+++ vshaurme/forms/main.py
@@ -1,18 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Optional, Length
+from flask_babel import lazy_gettext as _l
 
 
 class DescriptionForm(FlaskForm):
-    description = TextAreaField('Description', validators=[Optional(), Length(0, 500)])
-    submit = SubmitField()
+    description = TextAreaField(_l('Description'), validators=[Optional(), Length(0, 500)])
+    submit = SubmitField(_l('Submit'))
 
 
 class TagForm(FlaskForm):
-    tag = StringField('Add Tag (use space to separate)', validators=[Optional(), Length(0, 64)])
-    submit = SubmitField()
+    tag = StringField(_l('Add Tag (use space to separate)'), validators=[Optional(), Length(0, 64)])
+    submit = SubmitField(_l('Submit'))
 
 
 class CommentForm(FlaskForm):
     body = TextAreaField('', validators=[DataRequired()])
-    submit = SubmitField()
+    submit = SubmitField(_l('Submit'))
