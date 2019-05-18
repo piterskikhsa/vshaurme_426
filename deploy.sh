03b4fc3727d77400cc0c3307f16d69c01b34bb1d
Petr E. Antonov <antpeter@gmail.com>
2019-05-18 17:50:40 +0300

    Adding deploy-script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull
+. ~/vshaurme/venv/bin/activate
+
+pip install -r requirements.txt
+pybabel compile -d vshaurme/translations
+touch /var/www/www_vshaurme_su_wsgi.py
+echo "Deployed at $(date)" >> deploy.txt
