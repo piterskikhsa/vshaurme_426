47e9530543e4266debfd10b3abca844948e02ada
Petr E. Antonov <antpeter@gmail.com>
2019-05-20 22:05:14 +0300

    Fixing translation marking. Emails.

--- vshaurme/emails.py
+++ vshaurme/emails.py
@@ -2,6 +2,7 @@ from threading import Thread
 
 from flask import current_app, render_template
 from flask_mail import Message
+from flask_babel import _
 
 from vshaurme.extensions import mail
 
@@ -12,12 +13,12 @@ def send_mail(to, subject, template, **kwargs):
 
 
 def send_confirm_email(user, token, to=None):
-    send_mail(subject='Email Confirm', to=to or user.email, template='emails/confirm', user=user, token=token)
+    send_mail(subject=_('Email Confirm'), to=to or user.email, template='emails/confirm', user=user, token=token)
 
 
 def send_reset_password_email(user, token):
-    send_mail(subject='Password Reset', to=user.email, template='emails/reset_password', user=user, token=token)
+    send_mail(subject=_('Password Reset'), to=user.email, template='emails/reset_password', user=user, token=token)
 
 
 def send_change_email_email(user, token, to=None):
-    send_mail(subject='Change Email Confirm', to=to or user.email, template='emails/change_email', user=user, token=token)
+    send_mail(subject=_('Change Email Confirm'), to=to or user.email, template='emails/change_email', user=user, token=token)
