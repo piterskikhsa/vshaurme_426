acef949df44825842e71862e6e9a928d8f327b96
Petr E. Antonov <antpeter@gmail.com>
2019-05-20 22:23:07 +0300

    Adding Command-Line Enhancements for translations.

--- vshaurme/__init__.py
+++ vshaurme/__init__.py
@@ -178,3 +178,34 @@ def register_commands(app):
         click.echo('Generating %d comments...' % comment)
         fake_comment(comment)
         click.echo('Done.')
+
+    @app.cli.group()
+    def translate():
+        """Translation and localization commands."""
+        pass
+
+    @translate.command()
+    @click.argument('locale')
+    def init(locale):
+        """Initialize a new language."""
+        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+            raise RuntimeError('extract command failed')
+        if os.system(
+                'pybabel init -i messages.pot -d vshaurme/translations -l ' + locale):
+            raise RuntimeError('init command failed')
+        os.remove('messages.pot')
+
+    @translate.command()
+    def update():
+        """Update all languages."""
+        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+            raise RuntimeError('extract command failed')
+        if os.system('pybabel update -i messages.pot -d vshaurme/translations'):
+            raise RuntimeError('update command failed')
+        os.remove('messages.pot')
+
+    @translate.command()
+    def compile():
+        """Compile all languages."""
+        if os.system('pybabel compile -d vshaurme/translations'):
+            raise RuntimeError('compile command failed')
