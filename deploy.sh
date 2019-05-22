42c0a4ca23ae76af402e520ef7b36129360e17cf
Питерских Сергей Алеексевич <piterskihsa@alt.altarix.ru>
2019-05-22 08:44:18 +0300

    add rollbar deploy info

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,11 @@ pip install -r requirements.txt
 pybabel compile -d vshaurme/translations
 touch /var/www/www_vshaurme_su_wsgi.py
 echo "Deployed at $(date)" >> deploy.txt
+
+REVISION=`git log -n 1 --pretty=format:"%H"`
+ENVIRONMENT=production
+
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$API_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
