f66b542a3f2a55d3d191ea95d6a3da16e02a98c8
Питерских Сергей Алексеевич <spiterskih@kplatform.ru>
2019-05-22 09:00:17 +0300

    add revision output in deploy.txt

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ git pull
 pip install -r requirements.txt
 pybabel compile -d vshaurme/translations
 touch /var/www/www_vshaurme_su_wsgi.py
-echo "Deployed at $(date)" >> deploy.txt
 
 REVISION=`git log -n 1 --pretty=format:"%H"`
 ENVIRONMENT=production
@@ -16,3 +15,5 @@ curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$POST_ROLLBAR_TOKEN \
   -F environment=$ENVIRONMENT \
   -F revision=$REVISION
+
+echo "Deployed at $(date) $(REVISION)" >> deploy.txt
