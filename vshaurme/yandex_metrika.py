6730f8e6e56e79960e18d1aa8d5144b28c4df3ae
Sergey <piterskikhsa@mail.ru>
2019-06-11 14:05:46 +0300

    change count number

--- vshaurme/yandex_metrika.py
+++ vshaurme/yandex_metrika.py
@@ -1,14 +1,14 @@
 YM_TARGET_REGISTRATION_DICT = {
     'target_name': 'YM_TARGET_REGISTRATION',
-    'counter': 51838036,
+    'counter': 54014824,
 }
 
 YM_TARGET_IMAGE_UPLOAD_DICT = {
     'target_name': 'YM_TARGET_IMAGE_UPLOAD',
-    'counter': 51840859
+    'counter': 54014824
 }
 
 YM_TARGET_POSTING_COMMENT_DICT = {
     'target_name': 'YM_TARGET_POSTING_COMMENT',
-    'counter': 51842791
+    'counter': 54014824
 }
